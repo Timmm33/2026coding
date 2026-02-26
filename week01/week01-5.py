7cc710c3c27d6ce86c4c7a58e063aabf0d6935d4
Timmm33 <tim950303@gmail.com>
2026-02-26 16:36:08 +0800

    week01

---
+++ week01/week01-5.py
@@ -0,0 +1,9 @@
+## week01-4.cpp
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        ans = []
+        best = max(candies)
+        for candie in candies:
+            if candie + extraCandies >= best: ans.append(True)
+            else: ans.append(False)
+        return ans
